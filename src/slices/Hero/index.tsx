038a670868164a9097f745b677732fc7bd102cae
the-rudra <itsrudrapratapdev@gmail.com>
2024-04-02 04:18:02 +0530

    changed homepage

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -17,43 +17,43 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   const component = useRef(null);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      // create as many GSAP animations and/or ScrollTriggers here as you want...
-      gsap
-        .timeline()
-        .fromTo(
-          ".name-animation",
-          { x: -100, opacity: 0, rotate: -10 },
-          {
-            x: 0,
-            opacity: 1,
-            rotate: 0,
+  // useEffect(() => {
+  //   let ctx = gsap.context(() => {
+  //     // create as many GSAP animations and/or ScrollTriggers here as you want...
+  //     gsap
+  //       .timeline()
+  //       .fromTo(
+  //         ".name-animation",
+  //         { x: -100, opacity: 0, rotate: -10 },
+  //         {
+  //           x: 0,
+  //           opacity: 1,
+  //           rotate: 0,
 
-            ease: "elastic.out(1,0.3)",
-            duration: 1,
-            transformOrigin: "left top",
-            stagger: { each: 0.1, from: "random" },
-          },
-        )
-        .fromTo(
-          ".job-title",
-          {
-            y: 20,
-            opacity: 0,
-            scale: 1.2,
-          },
-          {
-            opacity: 1,
-            y: 0,
-            duration: 1,
-            scale: 1,
-            ease: "elastic.out(1,0.3)",
-          },
-        );
-    }, component);
-    return () => ctx.revert(); // cleanup!
-  }, []);
+  //           ease: "elastic.out(1,0.3)",
+  //           duration: 1,
+  //           transformOrigin: "left top",
+  //           stagger: { each: 0.1, from: "random" },
+  //         },
+  //       )
+  //       .fromTo(
+  //         ".job-title",
+  //         {
+  //           y: 20,
+  //           opacity: 0,
+  //           scale: 1.2,
+  //         },
+  //         {
+  //           opacity: 1,
+  //           y: 0,
+  //           duration: 1,
+  //           scale: 1,
+  //           ease: "elastic.out(1,0.3)",
+  //         },
+  //       );
+  //   }, component);
+  //   return () => ctx.revert(); // cleanup!
+  // }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
     if (!name) return;
@@ -68,34 +68,35 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   };
 
   return (
-    <Bounded
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      ref={component}
-      className="overflow-x-hidden"
-    >
-      <div className="grid grid-cols-1 items-center md:grid-cols-2">
-        <Shapes />
-        <div className="col-start-1 md:row-start-1 " data-speed=".2">
-          <h1
-            className="mb-8 text-[clamp(3rem,15vmin,15rem)] font-extrabold leading-none tracking-tighter"
-            aria-label={
-              slice.primary.first_name + " " + slice.primary.last_name
-            }
-          >
-            <span className="block text-slate-300 ">
-              {renderLetters(slice.primary.first_name, "first")}
-            </span>
-            <span className="-mt-[.2em] block text-slate-500  ">
-              {renderLetters(slice.primary.last_name, "last")}
-            </span>
-          </h1>
-          <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
-            {slice.primary.tag_line}
-          </span>
-        </div>
-      </div>
-    </Bounded>
+    // <Bounded
+    //   data-slice-type={slice.slice_type}
+    //   data-slice-variation={slice.variation}
+    //   ref={component}
+    //   className="overflow-x-hidden"
+    // >
+    //   <div className="grid grid-cols-1 items-center md:grid-cols-3 2xl:mx-8">
+    //     <Shapes />
+    //     <div className="col-start-1 md:row-start-1 " data-speed=".2">
+    //       <h1
+    //         className="mb-8 font-extrabold leading-none tracking-tighter md:text-[clamp(2rem,10vmin,10rem)] xl:text-[clamp(3rem,15vmin,15rem)]"
+    //         aria-label={
+    //           slice.primary.first_name + " " + slice.primary.last_name
+    //         }
+    //       >
+    //         <span className="block text-slate-300 ">
+    //           {renderLetters(slice.primary.first_name, "first")}
+    //         </span>
+    //         <span className="-mt-[.2em] block text-slate-500  ">
+    //           {renderLetters(slice.primary.last_name, "last")}
+    //         </span>
+    //       </h1>
+    //       <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-2xl xl:text-4xl">
+    //         {slice.primary.tag_line}
+    //       </span>
+    //     </div>
+    //   </div>
+    // </Bounded>
+    <Shapes />
   );
 };
 
